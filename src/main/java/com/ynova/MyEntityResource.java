ddcc07c6cc3895be9a01c6cf524cf9e73f8aaa08
quarkusio <no-reply@quarkus.io>
2025-10-22 08:12:19 +0000

    Initial commit

---
+++ src/main/java/com/ynova/MyEntityResource.java
@@ -0,0 +1,6 @@
+package com.ynova;
+
+import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
+
+public interface MyEntityResource extends PanacheEntityResource<MyEntity, Long> {
+}
